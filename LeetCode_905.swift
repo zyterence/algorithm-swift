bc7e513667879814b28f39a83a8027188bcdde97
Yang <zyterence@gmail.com>
2019-04-27 22:28:52 +0800

    js sulotion for 905 & 922

--- LeetCode_905.swift
+++ LeetCode_905.swift
@@ -19,3 +19,17 @@ class Solution {
 // 而使用 for in 的运行时间只有 72 ms
 // Runtime: 72 ms, faster than 93.41% of Swift online submissions for Sort Array By Parity.
 // Memory Usage: 19.4 MB, less than 12.50% of Swift online submissions for Sort Array By Parity.
+
+
+// solution 2 by JavaScript
+var sortArrayByParity = function(A) {
+    for (var i=0, j=A.length-1; i<j; i++, j--) {
+        while (A[i]%2 == 0) if (i>=j) break; else i++;
+        while (A[j]%2 == 1) if (i>=j) break; else j--;
+        [A[i], A[j]] = [A[j], A[i]];
+    }
+    return A;
+};
+
+// Runtime: 84 ms, faster than 93.15% of JavaScript online submissions for Sort Array By Parity.
+// Memory Usage: 37.3 MB, less than 40.08% of JavaScript online submissions for Sort Array By Parity.
--- LeetCode_922.swift
+++ LeetCode_922.swift
@@ -20,3 +20,17 @@ class Solution {
 
 // Runtime: 196 ms, faster than 99.24% of Swift online submissions for Sort Array By Parity II.
 // Memory Usage: 20 MB, less than 5.26% of Swift online submissions for Sort Array By Parity II.
+
+
+// solution 2 by JavaScript
+var sortArrayByParityII = function(A) {
+    var j = 1;
+    for (var i=0, j=1; i<A.length; i+=2)
+        if (A[i] % 2 == 1) {
+            while (A[j] % 2 == 1) j += 2;
+            [A[i], A[j]] = [A[j], A[i]]
+        }
+    return A;
+};
+// Runtime: 120 ms, faster than 50.18% of JavaScript online submissions for Sort Array By Parity II.
+// Memory Usage: 38.2 MB, less than 98.85% of JavaScript online submissions for Sort Array By Parity II.
