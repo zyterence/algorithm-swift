98d629b3f470b7a7d52f5ab4a1a99382635eb449
Yang <zyterence@gmail.com>
2019-05-07 11:27:11 +0800

    fibonacci

---
+++ data-structure/Fibonacci.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+struct Fibonacci: IteratorProtocol, Sequence {
+    typealias Element = Int
+    
+    var state = (curr: 0, next: 1)
+    
+    mutating func next() -> Iterator.Element? {
+        let curr = state.curr
+        state = (curr: state.next, next: state.curr + state.next)
+        return curr
+    }
+}
+
+var fibA = Fibonacci()
+
+print(fibA.next())
+print(fibA.next())
+print(fibA.next())
+print(fibA.next())
+print(fibA.next())
+print(fibA.next())
